55c2941be8604999b9ef865400f5d19e0daecd4d
Orchaldir <orchaldir@gmail.com>
2022-11-15 22:59:22 +0100

    [#2] Use GenderOption & Gender

--- rpg_tools_core/src/model/character/mod.rs
+++ rpg_tools_core/src/model/character/mod.rs
@@ -1,4 +1,6 @@
-use crate::model::character::species::SpeciesId;
+use crate::model::character::gender::Gender;
+use crate::model::character::species::{Species, SpeciesId};
+use anyhow::{bail, Result};
 
 pub mod gender;
 pub mod manager;
@@ -31,13 +33,50 @@ impl From<usize> for CharacterId {
 pub struct Character {
     id: CharacterId,
     species: SpeciesId,
+    gender: Gender,
 }
 
 impl Character {
-    pub fn new<I: Into<CharacterId>, S: Into<SpeciesId>>(id: I, species: S) -> Self {
+    /// Creates a character, if valid:
+    ///
+    /// ```
+    ///# use rpg_tools_core::model::character::Character;
+    ///# use rpg_tools_core::model::character::gender::Gender::*;
+    ///# use rpg_tools_core::model::character::species::Species;
+    ///# use rpg_tools_core::model::character::species::gender::GenderOption::*;
+    /// let species = Species::new(32, "test", TwoGenders).unwrap();
+    ///
+    /// assert!(Character::new(11, &species, Female).is_ok());
+    /// assert!(Character::new(11, &species, Male).is_ok());
+    /// assert!(Character::new(11, &species, Genderless).is_err());
+    /// ```
+    pub fn new<I: Into<CharacterId>>(id: I, species: &Species, gender: Gender) -> Result<Self> {
+        let id = id.into();
+        if !species.gender_option().is_valid(gender) {
+            bail!(
+                "Character {} is invalid, because {:?} doesn't match the species's {:?}!",
+                id.0,
+                gender,
+                species.gender_option()
+            );
+        }
+
+        Ok(Self {
+            id,
+            species: species.get_id(),
+            gender,
+        })
+    }
+
+    pub fn simple<I: Into<CharacterId>, S: Into<SpeciesId>>(
+        id: I,
+        species: S,
+        gender: Gender,
+    ) -> Self {
         Self {
             id: id.into(),
             species: species.into(),
+            gender,
         }
     }
 
--- rpg_tools_core/src/model/character/species/mod.rs
+++ rpg_tools_core/src/model/character/species/mod.rs
@@ -1,3 +1,4 @@
+use crate::model::character::species::gender::GenderOption;
 use crate::model::name::Name;
 use anyhow::{Context, Result};
 
@@ -28,29 +29,47 @@ impl From<usize> for SpeciesId {
 pub struct Species {
     id: SpeciesId,
     name: Name,
+    gender_option: GenderOption,
 }
 
 impl Species {
-    pub fn new<I: Into<SpeciesId>, S: Into<String>>(id: I, name: S) -> Result<Self> {
+    pub fn new<I: Into<SpeciesId>, S: Into<String>>(
+        id: I,
+        name: S,
+        gender_option: GenderOption,
+    ) -> Result<Self> {
         let id = id.into();
         let name = name.into();
         let name = Name::new(name).with_context(|| format!("Failed to create species {}", id.0))?;
 
-        Ok(Self { id, name })
+        Ok(Self {
+            id,
+            name,
+            gender_option,
+        })
     }
 
     pub fn get_id(&self) -> SpeciesId {
         self.id
+    }
+
+    pub fn name(&self) -> &Name {
+        &self.name
+    }
+
+    pub fn gender_option(&self) -> GenderOption {
+        self.gender_option
     }
 }
 
 #[cfg(test)]
 pub mod tests {
     use super::*;
+    use GenderOption::*;
 
     #[test]
     fn test_new() {
-        assert!(Species::new(0, "Test").is_ok());
-        assert!(Species::new(SpeciesId::new(2), "Test2").is_ok());
+        assert!(Species::new(0, "Test", TwoGenders).is_ok());
+        assert!(Species::new(SpeciesId::new(2), "Test2", NoGender).is_ok());
     }
 }
